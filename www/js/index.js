f1a70957bde4523e3d49aa9c6648c55e2f474cfc
Charlesrcm <charlesrc@outlook.fr>
2023-02-21 12:50:07 +0100

    ajout de pizza.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,17 @@
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady);
 
+const pizzasTag = document.querySelector('#pizzas-list');
+const ingredients = [
+    "mozza",
+    "tomate",
+    "jambon",
+    "olives",
+    "raclette",
+];
+
+let ingredientsSelected = '';
 
 function onDeviceReady() {
-
-    // root
     const homeNav = document.querySelector('#home-nav');
     const homePage = document.querySelector('#home-page');
     homeNav.root = homePage;
@@ -14,145 +20,88 @@ function onDeviceReady() {
     const pizzasPage = document.querySelector('#pizzas-page');
     pizzasNav.root = pizzasPage;
 
-    const pizzasTag = document.querySelector('#pizzas-list');
     const btCreatePizza = document.querySelector('#bt-create-pizza');
 
-    async function addAlert() {
-        const alert = document.createElement('ion-alert');
-        alert.header = 'Ajouter une pizza';
-        alert.mode = 'ios';
+    btCreatePizza.addEventListener('click', promptPizza);
+}
+
+
+function createItemList(pizzaName, ingredients) {
+    const ionItem = document.createElement('ion-item');
+    ionItem.innerHTML = `
+    <ion-label>
+      <h1>${pizzaName}</h1>
+      <h3>(${ingredients})</h3>
+    </ion-label>
+  `;
+    pizzasTag.insertBefore(ionItem, pizzasTag.firstElementChild);
+}
+
+async function promptPizza(title = 'Choisissez vos ingrédients', firstLaunch = true) {
+    const alert = document.createElement('ion-alert');
+    alert.header = title;
+
+    if (firstLaunch) {
+        alert.backdropDismiss = false;
+        alert.inputs = ingredients.map(ingredient => ({
+            label: capitalizeFirstLetter(ingredient),
+            value: ingredient,
+            type: "checkbox",
+        }));
+        alert.buttons = [
+            {
+                text: "Ok",
+                handler: (ingredients) => {
+                    // ici ingredients récupère les valeurs ingredient indiqué plus haut
+                    ingredientsSelected = formatIngredients(ingredients);
+                    return !!ingredients.length;
+                    // si on inverse deux la valeur devient bool en locurence false=0 true>=1
+                }
+            }
+        ]
+
+    } else {
         alert.inputs = [
             {
                 type: 'text',
-                name: 'nomPizza',
-                id: 'nomPizza',
-                placeholder: 'ajouter un nom',
-            },
-            {
-                type: 'text',
-                name: 'ingredients',
-                id: 'ingredients',
-                placeholder: 'Ajouter les ingrédients',
-            },
+                name: 'pizzaName',
+                placeholder: 'Nouvelle pizza',
+                attibutes: {
+                    maxlength: 50
+                }
+            }
         ];
         alert.buttons = [
             {
-                text: "OK",
-                role: 'confirm',
-                handler: (value) => {
-                    const ionItem = document.createElement('ion-item');
-                    ionItem.innerHTML = `
-                    <ion-label>
-                        <h2>${capitalizeFirst(nomPizza.value)}</h2>
-                        <h3>${formatIngredients(ingredients.value)}</h3>
-                    </ion-label>
-                    `;
-                    pizzasTag.insertBefore(ionItem, pizzasTag.firstElementChild);
+                text: 'Go !',
+                handler: ({ pizzaName }) => {
+                    if (!isValid(pizzaName)) return false;
+                    createItemList(
+                        capitalizeFirstLetter(pizzaName),
+                        ingredientsSelected,
+                    );
                 }
-            }];
-
-        document.body.appendChild(alert);
-        await alert.present();
+            }
+        ];
     }
-    btCreatePizza.addEventListener('click', addAlert)
-
-    // const pizzaForm = document.querySelector('#pizzaForm');
-    // const pizzasTag = document.querySelector('.pizzas');
-    // const errorTag = document.querySelector('.error');
-
-    // pizzaForm.addEventListener('submit', e => {
-    //     e.preventDefault();
-    //     const pizzaName = pizzaForm.nomPizza.value;
-    //     const ingredients = pizzaForm.ingredient.value;
-
-    //     if (isValid(pizzaName) && isValid(ingredients)) {
-
-    //         const pizzaTag = document.createElement('ion-col');
-
-    //         pizzaTag.setAttribute('size', '6');
-    //         pizzaTag.className = 'pizza';
-    //         pizzaTag.innerHTML = `
-    //                 <ion-card id="pizzaCard">
-    //                     <img alt="image de la pizza" src="../imports/pizza.jpg" />
-    //                     <ion-card-header>
-    //                     <ion-card-title>${capitalizeFirst(pizzaName)}</ion-card-title>
-    //                     </ion-card-header>
-
-    //                     <ion-card-content>
-    //                     ${formatIngredients(ingredients)}
-    //                     </ion-card-content>
-    //                     <ion-button fill="clear" id="delete" onclick="presentAlert()">
-    //                         <ion-icon name="trash-bin" color="danger"></ion-icon>
-    //                     </ion-button>
-    //                 </ion-card>
-    //         `;
 
 
-    //         pizzasTag.insertBefore(
-    //             pizzaTag,
-    //             pizzasTag.firsElementChild,
-    //         );
+    document.body.appendChild(alert);
+    await alert.present();
 
-    //         pizzaForm.requestFullscreen();
+    if (firstLaunch) {
+        alert.onDidDismiss().then(() => promptPizza('Créer une pizza', false));
+    }
 
-    //         // gestion de la suppression d'une pizza
-    //         const btDelete = document.querySelector('#delete');
-    //         btDelete.addEventListener('click', deleteAlert);
-
-    //         async function deleteAlert() {
-    //             const pizzaCard = document.querySelector('.pizza');
-
-    //             const alert = document.createElement('ion-alert');
-    //             alert.header = 'Attention vous allez supprimer la pizza!';
-    //             alert.buttons = [
-    //                 {
-    //                     text: 'Retour',
-    //                     role: 'cancel',
-    //                 },
-    //                 {
-    //                     text: 'OK',
-    //                     role: 'confirm',
-    //                     handler: () => { pizzaCard.remove(); }
-    //                 }
-    //             ];
-
-    //             document.body.appendChild(alert);
-    //             await alert.present();
-
-    //             const { role } = await alert.onDidDismiss();
-    //         }
-
-    //     } else {
-    //         errorTag.innerHTML = '<p>Formulaire invalide ! </p>';
-    //         setTimeout(() => errorTag.innerHTML = '', 2000);
-    //     }
-    // });
-
-
-    function capitalizeFirst(value) {
+    function capitalizeFirstLetter(value) {
         return value.trim().slice(0, 1).toUpperCase() + value.trim().slice(1);
     }
 
     function formatIngredients(value) {
-        return value.trim().replaceAll(' ', ', ');
+        return value.join(' ').trim().replaceAll(' ', ', ');
     }
 
     function isValid(value) {
         return value && value.length > 2 && value.length < 50;
     }
-
-    // const btAlert = document.querySelector('#bt-alert');
-    // btAlert.addEventListener('click', presentAlert);
 }
-
-// async function presentAlert() {
-//     const alert = document.createElement('ion-alert');
-//     alert.mode = 'ios';
-//     alert.header = 'Alert';
-//     alert.subHeader = 'Important message';
-//     alert.message = 'This is an alert!';
-//     alert.buttons = ['OK'];
-
-//     document.body.appendChild(alert);
-//     await alert.present();
-// }
